ad2a45dad87cb231755b1934f51907f896b712e2
codingarpan <contact.codingarpan@gmail.com>
2023-09-11 08:58:15 +0530

    size change

--- src/components/Table.Index.js
+++ src/components/Table.Index.js
@@ -3288,14 +3288,14 @@ function TableIndex() {
                         <p className="no text-2xl border-l-2 border-l-blue-700 pl-3 text-blue-800">{data.length}</p>
                     </div>
                 </div>
-                <div className="search m-auto mt-3 py-3 space-x-5  flex justify-start items-center border-b-2 border-b-blue-500 bg-gray-50 w-full ">
+                <div className="search m-auto mt-3 mob:py-3 py-2  space-x-5  flex justify-start items-center border-b-2 border-b-blue-500 bg-gray-50 w-full ">
                     <div className="icon p-2"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#3b82f6" /></svg></div>
                     <div className="input w-full">
                         <input
                             onChange={(e) => {
                                 searchname(e.target.value)
                             }}
-                            className='bg-transparent text-lg outline-none w-full' type="text" name="searchbar" id="searchbar" placeholder='Search Your Name Here' />
+                            className='bg-transparent mob:text-lg text-base outline-none w-full' type="text" name="searchbar" id="searchbar" placeholder='Search Your Name Here' />
                     </div>
                 </div>
             </div>
